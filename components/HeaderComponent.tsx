7680228038937efaab5071a6d8e8c7901a36a4d1
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-04 21:12:05 +0300

    Create header component

---
+++ components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+const HeaderComponent = () => {
+    return (
+        <nav className="hidden h-13  max-w-8xl bg-black lg:flex items-center  px-5 py-1.5 mx-7.5 relative z-30">
+            <p className="text-gray-200 text-[32px]">
+                Eltayara<span className="text-yellow-300">Feen</span>
+            </p>
+        </nav>
+    )
+}
+
+export default HeaderComponent
